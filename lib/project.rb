bf3a79959a97dbcd1c41b93b95defd890ea3ca0d
Mark Helt <heltmm@gmail.com>
2017-09-22 09:42:37 -0700

    create all and save methods for project class

--- lib/project.rb
+++ lib/project.rb
@@ -5,4 +5,22 @@ class Project
     @id = project_hash[:id]
   end
 
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects")
+    projects = []
+    returned_projects.each do |project|
+      projects.push(Project.new({title: project['title'], id: project['id']}))
+    end
+    projects
+  end
+
+  def save
+    @id = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING ID").first['id'].to_i
+  end
+
+  def ==
+
+  end
+
+
 end
