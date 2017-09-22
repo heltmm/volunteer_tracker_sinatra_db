a2fd5c75cfcc27f4227d54965c0559e0932bf364
Mark Helt <heltmm@gmail.com>
2017-09-22 10:31:46 -0700

    add save method to volunteers class

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -10,14 +10,14 @@ class Volunteer
   def self.all
     returned_volunteers = DB.exec("SELECT * FROM volunteers")
     volunteers = []
-    returned_projects.each do |volunteer|
-      volunteers.push(Volunteer.new(name: volunteer['name'], id: volunteer['id'].to_i, project_id: volunteer['project_id']))
+    returned_volunteers.each do |volunteer|
+      volunteers.push(Volunteer.new(name: volunteer['name'], id: volunteer['id'].to_i, project_id: volunteer['project_id'].to_i))
     end
     volunteers
   end
 
   def save
-
+    @id  = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING ID").first['id'].to_i
   end
 
   def ==(another_volunteer)
